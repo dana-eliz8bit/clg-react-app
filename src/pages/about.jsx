46a060cf9e19f6e26569de2b738d10a6e3010dfd
dana.eliz8bit <94809868+dana-eliz8bit@users.noreply.github.com>
2022-11-29 07:29:45 +1000

    Updated nav and footer components.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,6 +2,8 @@ const About = () => {
   return (
     <section>
       <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut vitae nobis omnis illum rerum magni enim deleniti facilis, neque consequatur libero necessitatibus sed velit delectus corrupti exercitationem recusandae minima error!
+      </p>
     </section>
   );
 };
